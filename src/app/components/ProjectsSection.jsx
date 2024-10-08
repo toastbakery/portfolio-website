53c2f0dbda27bdb5d1c5d0d6f9c4351890fcf4a5
noranora <348207971@qq.com>
2024-10-08 01:47:32 +0100

    modify: component path in ProjectSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
-import ProjectTag from "./projectTag";
+import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
